0239e47f4ff658f9aacaeabe6f0416beaac8d8ab
nickklosterman <nick_klosterman@yahoo.com>
2014-09-17 16:16:13 -0400

    comment out html so that jQueryUI isn't included so that the page isn't tabified

--- StockTrackerJSON.py
+++ StockTrackerJSON.py
@@ -1214,7 +1214,7 @@ def createHTMLOutput(portfolioName,portfolioNameList,portfolioContentList):
     <title> %s %s </title>
     <link rel="stylesheet" href="http://code.jquery.com/ui/1.10.3/themes/smoothness/jquery-ui.css" />
     <script src="http://code.jquery.com/jquery-1.9.1.js"></script>
-    <script src="http://code.jquery.com/ui/1.10.3/jquery-ui.js"></script>
+    <!-- <script src="http://code.jquery.com/ui/1.10.3/jquery-ui.js"></script> uncomment this to go back to using the jquery tabs-->
     <link rel="stylesheet" href="/resources/demos/style.css" />
     <script>
     $(function() {
